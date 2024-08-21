f72d258045942a811ceb6a9512be2a5a8359f518
Gaurav Rana <em.gauravrana@gmail.com>
2024-08-21 12:05:07 +0530

    update the getCatagories api route

--- src/app/api/blog/getcatagories/route.ts
+++ src/app/api/blog/getcatagories/route.ts
@@ -2,6 +2,14 @@ import prisma from "@/app/database/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  const categories = await prisma.categories.findMany();
-  return NextResponse.json(categories);
+  try {
+    const categories = await prisma.categories.findMany();
+    return NextResponse.json(categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch categories" },
+      { status: 500 }
+    );
+  }
 }
